4a72799425ef24d0f7eaf9defad550edbfe7247d
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-02-05 19:11:49 +0530

    added confiq for tailwind and shadcn-vue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: './postcss.config.js',
+  },
   plugins: [
     vue(),
   ],
