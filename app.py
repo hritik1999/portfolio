46a2a380373309d3fd45fcf1a63be07fedff9713
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-02-15 20:26:30 +0530

    fixing cors

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def change_tone():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5001, ssl_context=("cert.pem", "key.pem"))
